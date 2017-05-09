8c7cb6495aa58a212b5145a0fe54b45b2fee73f0
Jason Corbett <jcorbett@expedia.com>
2017-05-09 15:42:38 +1000

    Init commit

---
+++ resources/players.js
@@ -0,0 +1,104 @@
+var players = [
+    {
+        "name": "Alex Wills",
+        "alias": "Alex",
+        "id": "alex",
+        "image": "alex.jpg"
+    },
+    {
+        "name": "CJ Watson",
+        "alias": "CJ",
+        "id": "cj",
+        "image": "cj.jpg"
+    },
+    {
+        "name": "Gyaraki Claudiu",
+        "alias": "Claude",
+        "id": "claude",
+        "image": "claude.jpg"
+    },
+    {
+        "name": "Daniel Cooper",
+        "alias": "Daniel",
+        "id": "daniel",
+        "image": "daniel.jpg"
+    },
+    {
+        "name": "Daniel Ailoae",
+        "alias": "Danny",
+        "id": "danny",
+        "image": "danny.jpg"
+    },
+    {
+        "name": "Emi Asuenci",
+        "alias": "Emi",
+        "id": "emi",
+        "image": "emi.jpg"
+    },
+    {
+        "name": "Greg Bennett",
+        "alias": "Greg",
+        "id": "greg",
+        "image": "greg.jpg"
+    },
+    {
+        "name": "Jason Corbett",
+        "alias": "Jason",
+        "id": "jason",
+        "image": "jason.jpg"
+    },
+    {
+        "name": "Jeffrey Le",
+        "alias": "Jeff",
+        "id": "jeff",
+        "image": "jeff.jpg"
+    },
+    {
+        "name": "Tarlent Kokeh",
+        "alias": "Kevin",
+        "id": "kevin",
+        "image": "kevin.jpg"
+    },
+    {
+        "name": "Nachi Padmanabha",
+        "alias": "Kutty",
+        "id": "kutty",
+        "image": "kutty.jpg"
+    },
+    {
+        "name": "Amit Jamble",
+        "alias": "Mitty",
+        "id": "mitty",
+        "image": "mitty.jpg"
+    },
+    {
+        "name": "Ryan Arthurs",
+        "alias": "Ryan",
+        "id": "ryan",
+        "image": "ryan.jpg"
+    },
+    {
+        "name": "Sam Brew",
+        "alias": "Sam",
+        "id": "sam",
+        "image": "sam.jpg"
+    },
+    {
+        "name": "Tai Vo",
+        "alias": "Tai",
+        "id": "Tai",
+        "image": "tai.jpg"
+    },
+    {
+        "name": "Octavian Cornel Vaduva",
+        "alias": "Tavi",
+        "id": "tavi",
+        "image": "tavi.jpg"
+    },
+    {
+        "name": "Antonio Casimiro",
+        "alias": "Tony",
+        "id": "Tony",
+        "image": "tony.jpg"
+    }
+]
