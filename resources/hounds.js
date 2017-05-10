8653a067eda47f4cf20a5f3bc4a4248badc2fd7a
Jason Corbett <jcorbett@expedia.com>
2017-05-10 13:57:40 +1000

    Add hounds

---
+++ resources/hounds.js
@@ -0,0 +1,151 @@
+var players = [
+    {
+        "name": "Joe Henderson",
+        "alias": "Joe",
+        "id": "joe",
+        "image": "joe.jpg"
+    },
+    {
+        "name": "Chris Stagg",
+        "alias": "Chris",
+        "id": "chris",
+        "image": "chris.jpg"
+    },
+    {
+        "name": "Chris Chapman",
+        "alias": "Chapman",
+        "id": "chapman",
+        "image": "chrischapman.jpg"
+    },
+    {
+        "name": "Pat Whitton",
+        "alias": "Pat",
+        "id": "pat",
+        "image": "pat.jpg"
+    },
+    {
+        "name": "Max Williams",
+        "alias": "Max",
+        "id": "max",
+        "image": "max.jpg"
+    },
+    {
+        "name": "Marc Webb",
+        "alias": "Webby",
+        "id": "marc",
+        "image": "marc.jpg"
+    },
+    {
+        "name": "Mat Doherty",
+        "alias": "Mat",
+        "id": "mat",
+        "image": "mat.jpg"
+    },
+    {
+        "name": "Michael O'Brien",
+        "alias": "Pat",
+        "id": "pat",
+        "image": "pat.jpg"
+    },
+    {
+        "name": "Jordan Mathers",
+        "alias": "Jordan",
+        "id": "jordan",
+        "image": "jordan.jpg"
+    },
+    {
+        "name": "Andrew Latimore",
+        "alias": "Andrew",
+        "id": "andrew",
+        "image": "andrew.jpg"
+    },
+    {
+        "name": "Daniel Selff",
+        "alias": "Daniel",
+        "id": "selff",
+        "image": "danielselff.jpg"
+    },
+    {
+        "name": "Steve Whitton",
+        "alias": "Steve",
+        "id": "steve",
+        "image": "steve.jpg"
+    },
+    {
+        "name": "Riley Fegan",
+        "alias": "Riley",
+        "id": "riley",
+        "image": "riley.jpg"
+    },
+    {
+        "name": "Michael Hinchcliffe",
+        "alias": "Michael",
+        "id": "michael",
+        "image": "michael.jpg"
+    },
+    {
+        "name": "Jack Clark",
+        "alias": "Jack",
+        "id": "jack",
+        "image": "jackclark.jpg"
+    },
+    {
+        "name": "Ben Faulkner",
+        "alias": "Faulks",
+        "id": "faulkner",
+        "image": "faulkner.jpg"
+    },
+    {
+        "name": "Mitch Humphris",
+        "alias": "Mitch",
+        "id": "mitch",
+        "image": "mitch.jpg"
+    },
+    {
+        "name": "Scott Idec",
+        "alias": "Scott",
+        "id": "scott",
+        "image": "scott.jpg"
+    },
+    {
+        "name": "Eli Kailahi",
+        "alias": "Eli",
+        "id": "eli",
+        "image": "eli.jpg"
+    },
+    {
+        "name": "Rory McHugh",
+        "alias": "Rory",
+        "id": "rory"
+    },
+    {
+        "name": "Gio Mulitalo",
+        "alias": "Gio",
+        "id": "gio",
+        "image": "gio.jpg"
+    },
+    {
+        "name": "Beau Ryan",
+        "alias": "Beau",
+        "id": "beau",
+        "image": "beau.jpg"
+    },
+    {
+        "name": "Karl Scott",
+        "alias": "Karl",
+        "id": "karl",
+        "image": "karl.jpg"
+    },
+    {
+        "name": "Ryan Currie",
+        "alias": "Ryan",
+        "id": "ryan",
+        "image": "currie.jpg"
+    },
+    {
+        "name": "Ari Johnson",
+        "alias": "Ari",
+        "id": "ari",
+        "image": "ari.jpg"
+    }
+]
