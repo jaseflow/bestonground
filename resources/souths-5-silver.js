b06b1503f4c6c5759f32df33d4d5e9e34e8b4429
Jason Corbett <jcorbett@expedia.com>
2017-05-10 12:15:23 +1000

    Add Silver team

---
+++ resources/souths-5-silver.js
@@ -0,0 +1,91 @@
+var players = [
+    {
+        "name": "Akaash Singh",
+        "alias": "Akaash",
+        "id": "akaash",
+        "image": "akaash.jpg"
+    },
+    {
+        "name": "Aaron Carlill",
+        "alias": "Aaron",
+        "id": "aaron",
+        "image": "aaron.jpg"
+    },
+    {
+        "name": "Daniel Clarke",
+        "alias": "DC",
+        "id": "dc",
+        "image": "dc.jpg"
+    },
+    {
+        "name": "Jack Clarke",
+        "alias": "Jack",
+        "id": "jack",
+        "image": "jack.jpg"
+    },
+    {
+        "name": "Ben Stinton",
+        "alias": "Ben",
+        "id": "ben",
+        "image": "ben.jpg"
+    },
+    {
+        "name": "Matthew Reddell",
+        "alias": "Matty",
+        "id": "matt",
+        "image": "matt.jpg"
+    },
+    {
+        "name": "Tor Demmers",
+        "alias": "Tor",
+        "id": "tor",
+        "image": "tor.jpg"
+    },
+    {
+        "name": "Braydon Fernandez",
+        "alias": "Braydon",
+        "id": "braydon",
+        "image": "braydon.jpg"
+    },
+    {
+        "name": "Carlos Gavin Burdett",
+        "alias": "Carlos",
+        "id": "carlos",
+        "image": "carlos.jpg"
+    },
+    {
+        "name": "Tom Davey",
+        "alias": "Tom",
+        "id": "tom",
+        "image": "tom.jpg"
+    },
+    {
+        "name": "Jacob Wilkinson",
+        "alias": "Jake",
+        "id": "jake",
+        "image": "jake.jpg"
+    },
+    {
+        "name": "David Knight",
+        "alias": "Knighty",
+        "id": "knighty",
+        "image": "knighty.jpg"
+    },
+    {
+        "name": "Ami Schwarz",
+        "alias": "Ami",
+        "id": "ami",
+        "image": "ami.jpg"
+    },
+    {
+        "name": "David Cantatore",
+        "alias": "Dave C",
+        "id": "dave",
+        "image": "dave.jpg"
+    },
+    {
+        "name": "Robert Judge",
+        "alias": "Judgey",
+        "id": "judgey"
+    }
+]
